1c553e5daa1f4a012e52b1ee4834a3af23f7754e
oyamasiphula <oyama@projectcodex.co>
2017-06-22 10:38:58 +0200

    added json module as a dependancy with the more dynamic routes

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
 var express = require('express'),
     exphbs = require('express-handlebars'),
+    bodyParser = require('body-parser'),
     mongoose = require('mongoose');
 
 
 var app = express();
+var jsonParser = bodyParser.json()
 
-app.engine("handlebars", exphbs({defaultLayout : "main.handlebars" }));
+app.engine("handlebars", exphbs({
+    defaultLayout: "main.handlebars"
+}));
 app.set("view engine", "handlebars");
+
+var urlencodedParser = bodyParser.urlencoded({
+    extended: false
+})
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
     res.render("home");
 });
 //eg. dynamically routing
-app.get("/home", function (req, res) {
+app.get("/home", (req, res) => {
     res.redirect("/");
 });
 
-app.get("/greet",function(req, res){
-  res.render("greet")
+app.get("/greet", (req, res) => {
+    res.render("greet");
+});
+
+app.post("/greet", (req, res) => {
+    var name = req.body.specifyName;
+    console.log(name);
+    res.send(name);
 })
 
 var port = process.env.port || 3000;
 
-app.listen( port, function(){
-  console.log('listening on *:' + port);
+app.listen(port, () => {
+    console.log('listening on *:' + port);
 });
