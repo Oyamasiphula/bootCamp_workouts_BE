23e3e860b3e1f21aa6b8aa84ceddfa140bf9a2e9
oyamasiphula <oyama@projectcodex.co>
2017-06-22 16:33:01 +0200

    latest working code

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     exphbs = require('express-handlebars'),
     bodyParser = require('body-parser'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    greetUtil = require("./routes/greet.js");
 
 
 var app = express();
@@ -29,9 +30,6 @@ app.get("/home", (req, res) => {
 });
 
 app.get("/greet", (req, res) => {
-    //var name = req.body.specifyName;
-    //console.log(name);
-    //console.log(req.body.submit);
     res.render("greet");
 });
 
@@ -39,18 +37,15 @@ var greetedPeople = [];
 
 app.post("/greet", (req, res) => {
 
-    var name = req.body.specifyName;
-    var language = req.body.languages;
+    var actualName = req.body.specifyName;
+    var actualLang = req.body.languages;
     var count = 0;
-
-    greetedPeople.push({
-        "name": name
-    });
 
     console.log(greetedPeople);
     res.render('greet', {
-      greetedPerson : greetedPeople
+      greetedPerson : greetUtil.greet(actualLang, actualName)
     });
+
 });
 
 var port = process.env.port || 3000;
