6f9799f1a0629bf2ce55e0e9645ad2f5a3810c02
oyamasiphula <oyama@projectcodex.co>
2017-06-23 12:36:16 +0200

    added counter key and its value as a function call

--- routes/greet-routes.js
+++ routes/greet-routes.js
@@ -3,11 +3,10 @@ const greetPersonUtil = require("./greet.js");
 exports.greetRouter = (req, res) => {
 
     var actualName = req.body.specifyName;
-    console.log(actualName);
     var actualLang = req.body.languages;
-    console.log(actualLang);
 
     res.render('greet', {
-        greetedPerson: greetPersonUtil.greet(actualLang, actualName)
+        greetedPerson : greetPersonUtil.greetFactory(actualLang, actualName).greet(),
+        counter : greetPersonUtil.greetFactory(actualLang, actualName).counter()
     });
 }
