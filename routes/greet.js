8c19692ad5a55df2e3e519d88bd00f659c1565b5
oyamasiphula <oyama@projectcodex.co>
2017-07-07 13:48:36 +0200

    added latest changes

--- routes/greet.js
+++ routes/greet.js
@@ -1,34 +1,36 @@
 var greetedPerson = {};
+var counterVal = 0;
 
 exports.greetFactory = (langRadioOpt, name) => {
-    if (name === "") {
-      alert("Please enter a valid name");
-      return
-    }
+  if (name === "") {
+    alert("Please enter a valid name");
+    return
+  }
 
-    var counter = (name) => {
-        if (greetedPerson[name] === undefined) {
-            // greetedPerson[name] = 0;
-            return greetedPerson[name] = 0;
-        }
-        greetedPerson[name] += 1;
-        return greetedPerson[name];
+  var counter = (name) => {
+    if (greetedPerson[name] === undefined) {
+      let counterVal = 0;
+      greetedPerson[name] = counterVal;
     }
+    counterVal++
+    greetedPerson[name] = counterVal;
+    return greetedPerson[name];
+  }
 
-    var greet = () => {
-        if (langRadioOpt === "Sotho") {
-            return "Dumela, " + name;
-        };
-        if (langRadioOpt === "Xhosa") {
-            return "Molo, " + name;
-        };
-        if (langRadioOpt === "English") {
-            return "Hello, " + name;
-        };
-    }
-
-    return {
-          counter,
-          greet
+  var greet = () => {
+    if (langRadioOpt === "Sotho") {
+      return "Dumela, " + name;
     };
+    if (langRadioOpt === "Xhosa") {
+      return "Molo, " + name;
+    };
+    if (langRadioOpt === "English") {
+      return "Hello, " + name;
+    };
+  }
+
+  return {
+    counter,
+    greet
+  };
 }
