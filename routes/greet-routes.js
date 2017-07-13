d239cae5cc2d063d8f62725542b3fa9c15eadbc9
oyamasiphula <oyama@projectcodex.co>
2017-07-13 13:22:44 +0200

    added function calls with parameters

--- routes/greet-routes.js
+++ routes/greet-routes.js
@@ -2,12 +2,11 @@ const greetPersonUtil = require("./greet.js");
 
 exports.greetRouter = (req, res) => {
 
-    var actualName = req.body.specifyName;
-    var actualLang = req.body.languages;
-    console.log(actualName);
+  var actualName = req.body.specifyName;
+  var actualLang = req.body.languages;
 
-    res.render('greet', {
-        greetedPerson : greetPersonUtil.greetFactory(actualLang, actualName).greet(),
-        counter : greetPersonUtil.greetFactory(actualLang, actualName).counter()
-    });
+  res.render('greet', {
+    greetedPerson: greetPersonUtil.greetFactory(actualLang, actualName).greet(actualName),
+    counter: greetPersonUtil.greetFactory(actualLang, actualName).counter(actualName)
+  });
 }
