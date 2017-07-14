9e189e4d91b3949bb0c8483000a10796d3fcbb12
oyamasiphula <oyama@projectcodex.co>
2017-07-14 23:29:49 +0200

    added name validation as a flash message and its key reference but not yet handled

--- routes/greet-routes.js
+++ routes/greet-routes.js
@@ -1,12 +1,16 @@
 const greetPersonUtil = require("./greet.js");
 
-exports.greetRouter = (req, res) => {
+exports.greetRouter = (req, res, next) => {
 
   var actualName = req.body.specifyName;
   var actualLang = req.body.languages;
+  
+  // Set a flash message by passing the key, followed by the value, to req.flash().
+  req.flash('message', 'Please enter a valid name!');
 
   res.render('greet', {
     greetedPerson: greetPersonUtil.greetFactory(actualLang, actualName).greet(actualName),
-    counter: greetPersonUtil.greetFactory(actualLang, actualName).counter(actualName)
+    counter: greetPersonUtil.greetFactory(actualLang, actualName).counter(actualName),
+    message: req.flash('message')
   });
 }
