fbfbf34ca3eec753756394a6e723215f102f29e7
oyamasiphula <oyama@projectcodex.co>
2017-07-21 00:00:22 +0200

    added the latest working code

--- routes/greet-routes.js
+++ routes/greet-routes.js
@@ -1,23 +1,38 @@
 const greetPersonUtil = require("./greet.js");
+const greetPersonDataMod = require("../dataPart/greet-model");
 
 exports.greetRouter = (req, res, cb) => {
+  try {
+    let actualName = req.body.specifyName;
+    let actualLang = req.body.languages;
+    let counter = greetPersonUtil.greetFactory(actualLang, actualName).counter()
 
-  var actualName = req.body.specifyName;
-  var actualLang = req.body.languages;
+    if (actualName.length >= 3) {
+      var greetPerson = new greetPersonDataMod.personGreetings({
+        "name": actualName,
+        "count": counter
+      });
 
-  if (actualName.length <= 3) {
-    // Set a flash message by passing the key, followed by the value, to req.flash().
-    req.flash('message', 'Please enter valid name');
-    // Then respond with the set connect-flash
-    res.render('greet', {
-      message: req.flash('message')
-    });
-    return;
-  } else {
-
-    res.render('greet', {
-      greetedPerson: greetPersonUtil.greetFactory(actualLang, actualName).greet(actualName),
-      counter: greetPersonUtil.greetFactory(actualLang, actualName).counter(actualName)
-    });
+      greetPerson.save(function(err, person) {
+        if (err) {
+          console.log(err);
+        }
+        res.redirect('/greet')
+      });
+      res.render('greet', {
+        greetedPerson: greetPersonUtil.greetFactory(actualLang, actualName).greet(actualName),
+        counter: counter
+      });
+    } else {
+      // Set a flash message by passing the key, followed by the value, to req.flash().
+      req.flash('message', 'Please enter valid name');
+      // targeting the flash message to reference it with mustache/ hhelpers
+      res.render('greet', {
+        message: req.flash('message')
+      });
+    };
+  } catch (e) {
+    console.log(e);
+    res.send(e);
   };
 };
