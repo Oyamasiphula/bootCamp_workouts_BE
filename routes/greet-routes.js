c5de7da657de605f2750d9dbaa22524c1c0f2db6
oyamasiphula <oyama@projectcodex.co>
2017-07-21 13:42:28 +0200

    added a code that allows me to add greeted users

--- routes/greet-routes.js
+++ routes/greet-routes.js
@@ -1,38 +1,40 @@
-const greetPersonUtil = require("./greet.js");
-const greetPersonDataMod = require("../dataPart/greet-model");
+const greetPersonUtil = require("./greet.js"),
+  greetPersonDataMod = require("../dataPart/greet-model");
 
-exports.greetRouter = (req, res, cb) => {
+exports.getGreet = (req, res) => {
+  let actualName = req.body.specifyName;
+  let actualLang = req.body.languages;
+
+  res.render('greet')
+
+  // res.render('greet', {
+  //   greetedPerson: greetPersonUtil.greetFactory(actualLang, actualName).greet(actualName),
+  //   counter: greetPersonUtil.greetFactory(actualLang, actualName).counter()
+  // });
+};
+
+exports.insertGreetData = (req, res) => {
   try {
     let actualName = req.body.specifyName;
     let actualLang = req.body.languages;
     let counter = greetPersonUtil.greetFactory(actualLang, actualName).counter()
 
+    console.log(actualName.length)
+
     if (actualName.length >= 3) {
       var greetPerson = new greetPersonDataMod.personGreetings({
         "name": actualName,
-        "count": counter
+        "counter": counter
       });
-
       greetPerson.save(function(err, person) {
         if (err) {
           console.log(err);
         }
         res.redirect('/greet')
       });
-      res.render('greet', {
-        greetedPerson: greetPersonUtil.greetFactory(actualLang, actualName).greet(actualName),
-        counter: counter
-      });
-    } else {
-      // Set a flash message by passing the key, followed by the value, to req.flash().
-      req.flash('message', 'Please enter valid name');
-      // targeting the flash message to reference it with mustache/ hhelpers
-      res.render('greet', {
-        message: req.flash('message')
-      });
     };
   } catch (e) {
     console.log(e);
-    res.send(e);
+    res.send(JSON.stringify(e));
   };
 };
